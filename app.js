9d1cff2e6a16ab67389f6ed973690b50e2c04fca
Matt Luker <jamesmattluker@gmail.com>
2022-03-27 12:00:34 -0600

    add role key for person object

--- app.js
+++ app.js
@@ -12,5 +12,5 @@ favActivities = ['Climbing'];
 console.log(person.name);
 for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
     var hobby = _a[_i];
-    console.log(hobby);
+    console.log(hobby + "Is my favorite hobby");
 }
--- app.ts
+++ app.ts
@@ -2,10 +2,18 @@
 //   name: string;
 //   age: number;
 // } = {
-  const person = {
+  const person: {
+    name: string;
+    age: number;
+    hobbies: string[];
+    //tuple below
+    role: [number, string];
+  }= {
   name: 'Matt',
   age: 30,
-  hobbies: ['Climbing', 'Running']
+  hobbies: ['Climbing', 'Running'],
+  //tuple below
+  role: [2, 'ecologist']
 };
 
 let favActivities: string[];
@@ -14,5 +22,5 @@ favActivities = ['Climbing'];
 console.log(person.name);
 
 for (const hobby of person.hobbies) {
-  console.log(hobby);
+  console.log(hobby + "Is my favorite hobby");
 }
