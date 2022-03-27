124ad711bb8ca7086d4c51315797d48cebec7a12
Matt Luker <jamesmattluker@gmail.com>
2022-03-27 13:52:39 -0600

    add generateError function to app.ts

--- app.js
+++ app.js
@@ -1,25 +1,9 @@
-function combine(input1, 
-//union types
-input2, 
-//literal types
-resultConversion) {
-    var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
-        result = +input1 + +input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    return result;
-    // if (resultConversion === 'as-number') {
-    //   return +result;
-    // } else {
-    //   return result;
-    // }
+var userInput;
+var userName;
+if (typeof userInput === 'string') {
+    userName = userInput;
 }
-var combinedAges = combine(30, 26, 'as-number');
-console.log(combinedAges);
-var combineStringAges = combine('30', '26', 'as-number');
-console.log(combineStringAges);
-var combinedNames = combine('Matt', 'Briana', 'as-text');
-console.log(combinedNames);
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+}
+generateError('An error occurred!', 500);
